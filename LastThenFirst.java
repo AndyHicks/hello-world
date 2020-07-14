0a24c8de29fcb393a2279f31bc986ddbb502c5c4
Mr. Hicks <andy_hicks@kernhigh.org>
2020-07-14 15:53:32 -0700

    Add new Java file

---
+++ LastThenFirst.java
@@ -0,0 +1,26 @@
+/*
+ * Program: LastThenFirst
+ * Date: July 6, 2020
+ * Programmer: Andy Hicks
+ */
+
+import java.util.Scanner;
+public class LastThenFirst
+{
+    public static void main(String[] args)
+    {
+        System.out.println("LastThenFirst\n"); 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please enter a name: ");
+        String theName = sc.nextLine();
+
+        System.out.println("\nThe original name: " + theName);
+
+        int nameLength = theName.length();
+        int halfLength = nameLength / 2;
+
+        String lastThenFirstName = theName.substring(halfLength) + theName.substring(0, halfLength);
+        System.out.println("\nThe LastThenFirst name: " + lastThenFirstName);
+    }
+}
