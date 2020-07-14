af527a14147746566e908aafd2c0a3d4d52aa4dc
Andy Hicks <mr.hicks.csa@gmail.com>
2020-07-14 15:47:27 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,6 +6,7 @@ public class Hello {
     System.out.println("Hello BIG & SMALL world!!!!");
     System.out.println("Oh my goodness!");
     System.out.println("VS Code changes");
+    // another addition from VS
 
   }
 
